80c811d47cebd6aa3af2fef29081c20eca637c64
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2010-01-18 07:18:05 +0000

    pass array object rather than address+length

--- spuhelper.c
+++ spuhelper.c
@@ -9,6 +9,30 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+static PyObject * spuhelper_index_image
+  (
+	PyObject * self,
+	PyObject * args
+  )
+  {
+	PyObject * result = 0;
+	unsigned long pixaddr, pixlen;
+	const uint32_t * pixels;
+	do /*once*/
+	  {
+		if (!PyArg_ParseTuple(args, "kk", &pixaddr, &pixlen))
+			break;
+		pixels = (const uint32_t *)pixaddr;
+		pixlen >>= 2;
+	  /* more TBD */
+	  /* all done */
+		Py_INCREF(Py_None);
+		result = Py_None;
+	  }
+	while (false);
+	return result;
+  } /*spuhelper_index_image*/
+
 static PyObject * spuhelper_cairo_to_gtk
   (
 	PyObject * self,
@@ -18,11 +42,19 @@ static PyObject * spuhelper_cairo_to_gtk
 	GTK Pixbuf (big-endian) ordering. */
   {
 	PyObject * result = 0;
+	PyObject * TheArray = 0;
+	PyObject * TheBufferInfo = 0;
 	unsigned long pixaddr, pixlen;
 	uint8_t * pixels;
 	do /*once*/
 	  {
-		if (!PyArg_ParseTuple(args, "kk", &pixaddr, &pixlen))
+		if (!PyArg_ParseTuple(args, "O", &TheArray))
+			break;
+		Py_INCREF(TheArray);
+		TheBufferInfo = PyObject_CallMethod(TheArray, "buffer_info", "");
+		if (TheBufferInfo == 0)
+			break;
+		if (!PyArg_ParseTuple(TheBufferInfo, "kk", &pixaddr, &pixlen))
 			break;
 		pixels = (uint8_t *)pixaddr;
 		pixlen >>= 2;
@@ -40,13 +72,23 @@ static PyObject * spuhelper_cairo_to_gtk
 		result = Py_None;
 	  }
 	while (false);
+	Py_XDECREF(TheBufferInfo);
+	Py_XDECREF(TheArray);
 	return result;
   } /*spuhelper_cairo_to_gtk*/
 
 static PyMethodDef spuhelper_methods[] =
   {
+#if 0
+	{"index_image", spuhelper_index_image, METH_VARRGS,
+		"index_image(address, nrbytes)\n"
+		"analyzes a buffer of RGBA-format pixels in Cairo (native-endian) ordering, and"
+		" returns a tuple of 2 elements, the first being a new array object containing"
+		" 2 bits per pixel, and the second being a tuple of corresponding colours."
+	},
+#endif
 	{"cairo_to_gtk", spuhelper_cairo_to_gtk, METH_VARARGS,
-		"cairo_to_gtk(address, nrbytes)\n"
+		"cairo_to_gtk(array)\n"
 		"converts a buffer of RGBA-format pixels from Cairo (native-endian) ordering"
 		" to GTK Pixbuf (big-endian) ordering."
 	},
