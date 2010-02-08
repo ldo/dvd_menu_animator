050be62907e9986a0b09781bb963d164d2e6fabe
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2010-02-08 09:32:58 +0000

    add licence

--- spuhelper.c
+++ spuhelper.c
@@ -2,7 +2,23 @@
 	Python extension module for DVD Menu Animator script. This performs
 	pixel-level manipulations that would be too slow done in Python.
 
-	Created by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+	Copyright 2010 Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+
+	This file is part of DVD Menu Animator.
+
+	DVD Menu Animator is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by the
+	Free Software Foundation, either version 3 of the License, or (at your
+	option) any later version.
+
+	DVD Menu Animator is distributed in the hope that it will be useful, but
+	WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+	or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+	for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with DVD Menu Animator. If not, see <http://www.gnu.org/licenses/>.
+
 */
 
 #include <Python.h>
